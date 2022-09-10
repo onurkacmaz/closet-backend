2e376c82560da87f708f3187f6a256693abca713
Onur Kaçmaz <onur.kacmaz@path.com.tr>
2022-09-11 02:44:47 +0300

    add translations

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -4,4 +4,5 @@ return [
     'INVALID_CREDENTIALS' => 'Invalid credentials.',
     'USER_NOT_FOUND' => 'User not found.',
     'INVALID_TOKEN' => 'Invalid token.',
+    'INVALID_SMS_CODE' => 'Invalid SMS code.',
 ];
